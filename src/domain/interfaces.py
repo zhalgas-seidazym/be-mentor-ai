6afe735c6abad451a69b5cee79782cfc8b675877
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2026-02-18 17:00:29 +0500

    added base interfaces

---
+++ src/domain/interfaces.py
@@ -0,0 +1,34 @@
+from typing import Protocol, Optional, Any, List
+
+from fastapi import UploadFile
+
+
+class IUoW(Protocol):
+    ...
+
+class IJWTService(Protocol):
+    def encode_token(
+        self,
+        data: dict,
+        expires_delta: Optional[int] = None,
+        is_access_token: bool = True
+    ) -> str: ...
+
+    def decode_token(self, token: str) -> dict: ...
+
+
+class IEmailService(Protocol):
+    async def send_email(
+        self,
+        to_email: str,
+        subject: str,
+        body: str,
+        html: bool = False
+    ) -> None: ...
+
+class IHashService(Protocol):
+    @staticmethod
+    def hash_password(password: str) -> str: ...
+
+    @staticmethod
+    def verify_password(password: str, hashed_password: str) -> bool: ...
